c4e6bb355c4f9e7204007207c780ba63ae32376e
Tushar Mehra <70595562+mehraTushar@users.noreply.github.com>
2024-10-06 21:44:24 +0530

    learned about dynamic routes params and query

--- src/app.js
+++ src/app.js
@@ -2,16 +2,31 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.end("this is the Test Route");
+app.get("/user/:userId", (req, res) => {
+  const { userId } = req.params;
+  console.log(userId);
+  res.send(`Got the UserId ==> ${userId}`);
 });
 
-app.use("/hello", (req, res) => {
-  res.end("this is the Hello Route");
+app.get("/user", (req, res) => {
+  const { userName, age } = req.query;
+  console.log(userName, age);
+
+  if (userName && age)
+    res.send(`got the userName ==> ${userName} and Age ==> ${age}`);
+  else if (userName) res.send(`got the userName ==> ${userName}`);
+  else if (age) res.send(`got the age ==> ${age}`);
+  else res.send("no params Passed sending    nothing");
 });
-app.use("/", (req, res) => {
-  res.end("home Page");
+
+app.post("/user", (req, res) => {
+  res.send("Post request To Users");
 });
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted the user");
+});
+
 app.listen(7777, () => {
   console.log("server Started");
 });
